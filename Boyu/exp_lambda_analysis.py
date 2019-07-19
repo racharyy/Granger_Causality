e9a7d902da9c9d28b4abc6fe1ede675ac9ddca72
cristianoBY <boyu2844240635@gmail.com>
2019-07-19 15:13:51 -0400

    exp distribution lambda feature vector done

--- Boyu/exp_lambda_analysis.py
+++ Boyu/exp_lambda_analysis.py
@@ -12,53 +12,6 @@ from io import open
 import scipy.stats as st
 
 
-# given all the user files in json
-# convert the time stamps between adjacent search histories into time difference (delta)
-def _get_delta_time(args):
-
-	# the time difference list for all people
-	total_delta_list = []
-
-	# per person
-	for file in os.scandir(args.data_path):
-		if file.name.endswith('.json'):
-			whole_name = args.data_path + file.name
-			# print(file.name)
-
-			# previous timestamp
-			# first place holder
-			previous = datetime.combine(date(2000, 1, 1), time(00, 00, 00))
-		
-			# the time difference list per person
-			delta_list = []
-
-			# per search history
-			with open(whole_name, 'r') as json_file:  
-				search_history = [json.loads(line) for line in json_file]
-
-				for instance in search_history:
-
-					# print(instance['qtime'])
-					date_time = _trim_date(instance['qtime'])
-
-					# get the time difference in minutes
-					delta = (previous - date_time).total_seconds()
-
-					if delta < args.threshold and delta > args.lower_bound:
-						delta_list.append(delta)
-
-					# print(delta)
-					previous = date_time
-
-			# remove the first placeholder
-			total_delta_list.append(delta_list[1:])
-
-	# return the flatten list
-	with open('./total_list.pkl', mode = 'wb') as f:
-		final_list = [val for sublist in total_delta_list for val in sublist]
-		print(len(final_list))
-		pickle.dump(final_list, f)
-
 # helper function
 # generate the date time instance from the raw string
 def _trim_date(qtime):
@@ -87,37 +40,6 @@ def _trim_date(qtime):
 	# print(new_time)
 
 	return datetime.combine(date, new_time)
-
-# plot the histogram for the distribution of delta time interval
-def plot_hist(args, file_path):
-
-	with open(file_path, mode = 'rb') as f:
-		total_delta_list = pickle.load(f)
-
-	# plot the raw version to get the log bins
-	f = plt.figure(1)
-	hist, bins, _ = plt.hist(total_delta_list, bins = 'auto')
-	plt.xlabel('Seconds')
-	plt.ylabel('Frequency')
-	title = 'Idle Time Between Searches (upper: ' + str(args.threshold) + ', lower: ' + str(args.lower_bound) + ')'
-	plt.title(title)
-	name = '../../upper_' + str(args.threshold) + '_lower_' + str(args.lower_bound) + '_abs'
-	plt.show()
-	f.savefig(name)
-
-	# get the log scale plot
-	logbins = np.logspace(np.log10(bins[0]), np.log10(bins[-1]), args.bin_size)
-	g = plt.figure(2)
-	plt.hist(total_delta_list, bins = logbins, density = False)
-	plt.xscale('log')
-
-	plt.xlabel('Seconds (log 10 based)')
-	plt.ylabel('Frequency')
-	title = 'Idle Time Between Searches (upper: ' + str(args.threshold) + ', lower: ' + str(args.lower_bound) + ')'
-	plt.title(title)
-	plt.show()
-	name = '../../upper_' + str(args.threshold) + '_lower_' + str(args.lower_bound)
-	g.savefig(name)
 
 
 # fit the data distribution to exponential distribution
@@ -155,8 +77,10 @@ def fit_exp(args, normed):
 						delta = (previous - date_time).total_seconds()
 
 						# error report
+						'''
 						if delta < 0 and previous != datetime.combine(date(2000, 1, 1), time(00, 00, 00)):
 							print(whole_name, idx, previous, date_time)
+						'''
 
 						delta_list.append(delta)
 						previous = date_time
@@ -196,6 +120,77 @@ def fit_exp(args, normed):
 # helper method: get the lambda for each peron by fitting exponential distribution
 def _fit_exp_per_person(delta_list):
 	return 1 / np.mean(np.asarray(delta_list))
+
+# get the mean and median lambda for exponential distribution
+# based on each category
+def mean_lambda_category(args, normed, categories):
+
+	# for each categories
+	for i, category in enumerate(categories):
+
+		# for both groups
+		all_param_dict = {'low':[], 'not_low':[]}
+		for group in ['low_self_esteem/', 'not_low_self_esteem/']:
+			path = args.data_path + group
+			param_list = []
+
+			# per peron 
+			for file in os.scandir(path):
+				if file.name.endswith('.json'):
+
+					whole_name = path + file.name
+					# print(file.name)
+
+					# previous timestamp, first place holder
+					previous = datetime.combine(date(2000, 1, 1), time(00, 00, 00))
+
+					# the time difference list per person
+					delta_list = []
+
+					# per search history
+					with open(whole_name, 'r') as json_file:  
+						search_history = [json.loads(line) for line in json_file]
+						for idx, instance in enumerate(search_history):
+
+							# only record the given category
+							for c in instance['category']:
+								current = c[0].split('/')[1]
+
+								if current == category:
+
+									# print(instance['qtime'])
+									date_time = _trim_date(instance['qtime'])
+
+									# get the time difference in minutes
+									delta = (previous - date_time).total_seconds()
+
+									# error report
+									'''
+									if delta < 0 and previous != datetime.combine(date(2000, 1, 1), time(00, 00, 00)):
+										print(whole_name, idx, previous, date_time)
+									'''
+
+									# print(delta)
+									delta_list.append(delta)
+									previous = date_time
+
+					# fit the exponential distribution for each category
+					# remove the first place holder
+					if len(delta_list[1:]) > 1:
+						param = _fit_exp_per_person(delta_list[1:])
+						param_list.append(param)
+					else:
+						print(group, file.name, category, 'only searched once')
+					
+			if group == 'low_self_esteem/':
+				all_param_dict['low'] = param_list
+			else:
+				all_param_dict['not_low'] = param_list
+			# print(path, category, 'done')
+
+		for key, value in all_param_dict.items():
+			print(category, key, np.mean(np.asarray(value)), np.median(np.asarray(value)))
+
 
 # fit the data distribution to exponential distribution
 # based on each category
@@ -241,8 +236,10 @@ def fit_exp_category(args, normed, categories):
 									delta = (previous - date_time).total_seconds()
 
 									# error report
+									'''
 									if delta < 0 and previous != datetime.combine(date(2000, 1, 1), time(00, 00, 00)):
 										print(whole_name, idx, previous, date_time)
+									'''
 
 									# print(delta)
 									delta_list.append(delta)
@@ -253,31 +250,20 @@ def fit_exp_category(args, normed, categories):
 					if len(delta_list[1:]) > 1:
 						param = _fit_exp_per_person(delta_list[1:])
 						param_list.append(param)
-					'''
 					else:
-						print(group, file.name, category, 'empty')
-					'''
-					
-			if len(param_list) == 0:
-				print(path, category, 'param list empty')
+						print(group, file.name, category, 'only searched once')
+
+			# remove outlier by 100 * median
+			x_100_median = np.median(np.asarray(param_list)) * 100
+			param_list = [p for p in param_list if p < x_100_median]
+
+			if group == 'low_self_esteem/':
+				all_param_dict['low'] = param_list
 			else:
+				all_param_dict['not_low'] = param_list
+			print(path, category, 'done')
 
-				# remove outlier
-				'''
-				x_100_median = np.median(np.asarray(param_list)) * 100
-				param_list = [p for p in param_list if p < x_100_median]
-				'''
-
-				if group == 'low_self_esteem/':
-					all_param_dict['low'] = param_list
-				else:
-					all_param_dict['not_low'] = param_list
-			# print(path, category, 'done')
-
-		for key, value in all_param_dict.items():
-			print(category, key, np.mean(np.asarray(value)), np.median(np.asarray(value)))
-
-		'''
+		# plot for each category
 		plt.figure(i, figsize = (16, 9))
 		if normed:
 			for key, value in all_param_dict.items():
@@ -288,16 +274,98 @@ def fit_exp_category(args, normed, categories):
 			for key, value in all_param_dict.items():
 				plt.hist(value, bins = 'auto', alpha = 0.5, label = key)
 				title = 'Lambda Distribution ' + category
-
 		plt.xlabel('Lambda')
 		plt.ylabel('Frequency')
 		plt.legend(loc = 'best')
 		plt.title(title)
 		plt.savefig(title, format='png', dpi=150, bbox_inches='tight')
 		plt.close()
-		'''
-		# print(category, 'all done')
+		print(category, 'all done')
 
+
+# extract the lambda feature vector for each person
+# 27-d category vector for each person
+def extract_lambda_feature(args, categories):
+
+	# for each categories
+	# each is 95 * 27
+	low_matrix = []
+	not_low_matrix = []
+
+	# for both groups
+	for group in ['low_self_esteem/', 'not_low_self_esteem/']:
+		path = args.data_path + group
+
+		# per peron 
+		for file in os.scandir(path):
+			if file.name.endswith('.json'):
+
+				# 27-d vector for the current user
+				category_vector = [] 
+
+				# for each categories
+				for i, category in enumerate(categories):
+
+					whole_name = path + file.name
+
+					# previous timestamp, first place holder
+					previous = datetime.combine(date(2000, 1, 1), time(00, 00, 00))
+
+					# the time difference list per person
+					delta_list = []
+
+					# per search history
+					with open(whole_name, 'r') as json_file:  
+						search_history = [json.loads(line) for line in json_file]
+						for idx, instance in enumerate(search_history):
+
+							# only record the given category
+							for c in instance['category']:
+								current = c[0].split('/')[1]
+
+								if current == category:
+
+									# print(instance['qtime'])
+									date_time = _trim_date(instance['qtime'])
+
+									# get the time difference in minutes
+									delta = (previous - date_time).total_seconds()
+
+									# error report
+									'''
+									if delta < 0 and previous != datetime.combine(date(2000, 1, 1), time(00, 00, 00)):
+										print(whole_name, idx, previous, date_time)
+									'''
+
+									# print(delta)
+									delta_list.append(delta)
+									previous = date_time
+
+					# fit the exponential distribution for each category
+					# remove the first place holder
+					if len(delta_list[1:]) > 1:
+						param = _fit_exp_per_person(delta_list[1:])
+						category_vector.append(param)
+					else:
+						print(group, file.name, category, 'only searched once')
+						category_vector.append(0)
+
+				# verify output and store away
+				assert len(category_vector) == 27
+				if group == 'low_self_esteem/':
+					low_matrix.append(category_vector)
+				else:
+					not_low_matrix.append(category_vector)
+
+		print(group, 'done')
+
+	low_matrix = np.stack(low_matrix)
+	not_low_matrix = np.stack(not_low_matrix)
+	print('low shape: {}, not low shape: {}'.format(low_matrix.shape, not_low_matrix.shape))
+
+	# return the 95 * 27 matrix
+	with open('./lambda_data_per_user_matrix.pkl', mode = 'wb') as f:
+		pickle.dump((low_matrix, not_low_matrix), f)
 
 def main():
 	parser = argparse.ArgumentParser(description = 'parser for data files')
@@ -315,8 +383,6 @@ def main():
 
 	args = parser.parse_args()
 
-	# _get_delta_time(args)
-	# plot_hist(args, './total_list.pkl')
 	# fit_exp(args, normed = True)
 
 	l = [
@@ -348,24 +414,8 @@ def main():
 	'People & Society', 
 	'Travel']
 
-	fit_exp_category(args, normed = True, categories = l)
-
-	'''
-	plt.figure(figsize = (16, 9))
-	all_param_dict = dict()
-	low = [1.1142918896083136e-06, 7.91804202656767e-06, 5.891356094011244e-06, 4.6009262237834666e-07, 4.7536074071603134e-07, 5.486761233136746e-06, 1.5380115656370372e-06, 3.6700165266318807e-06, 9.271777885250748e-07, 3.135677199071384e-06, 9.62782213479099e-06, 2.6791399609088363e-07, 3.7161757109582004e-06, 1.1499090421947625e-06, 1.0486534000115845e-05, 4.414343730424059e-06, 4.971621515751635e-06, 9.258860388275671e-07, 2.0320733792996853e-05, 2.5147137492730746e-06, 8.428417483458449e-07, 0.25, 4.864367305132299e-06, 1.8561179154589332e-06, 3.69462884234368e-06, 5.271674657686323e-06, 1.481136922260619e-06, 1.6811950036419494e-06, 3.1288902742211822e-06, 3.250287323806146e-07, 1.3685540963357778e-05, 9.412296111645844e-07, 1.0425500142057518e-06, 7.49712223345575e-07, 1.2272364661744837e-06, 1.5280165042939138e-06, 6.48904926807168e-06, 1.4137008000914877e-06, 6.2427545030548915e-06, 3.3816315698147386e-06, 9.370623654630318e-07, 7.947311693217815e-08, 3.5906128933088304e-06, 3.5710890732698923e-06, 5.891356094011244e-06, 1.9206111070752732e-05, 5.81695794891454e-06, 9.016637215789035e-06, 4.204321622195455e-06, 2.27498088936173e-06, 1.6821148775194555e-06]
-	not_low = [3.0289521465043043e-06, 2.247627091440751e-06, 1.3033548703866734e-06, 1.115020598060746e-06, 2.9188859433966395e-07, 5.9714404619548495e-06, 5.8976657611733174e-06, 3.2948103296062355e-07, 1.937231401830799e-06, 1.0508268364360798e-06, 2.6308381587289896e-06, 4.911735970949361e-06, 1.527314383377578e-06, 5.893869099131931e-06, 2.993900539082012e-06, 2.2095098553025618e-05, 1.1445818765739486e-06, 1.937231401830799e-06, 1.0440486743585387e-05, 5.126952165365398e-06, 2.415051645154916e-06, 4.010518227875447e-06, 6.461944537481227e-07, 1.4306776324252854e-06, 1.0640795063181848e-06, 6.039782157640453e-07, 1.1891568634061295e-06, 1.121912636662983e-05, 1.609536642567496e-06, 1.8124519269289515e-06, 6.48857203519104e-06, 2.7959828638724268e-06, 6.613784462804947e-07, 2.7629226933029145e-06, 9.915259606313298e-06, 1.3208526145894458e-07, 1.1832777702002867e-06, 2.7463074514147864e-06]
-	all_param_dict['low'] = low
-	all_param_dict['not_low'] = not_low
-	for key, value in all_param_dict.items():
-		plt.hist(value, bins = 'auto', alpha = 0.5, label = key)	
-	plt.xlabel('Lambda')
-	plt.ylabel('Frequency')
-	plt.legend(loc = 'best')
-	plt.title('title')
-	plt.savefig('title', format='png', dpi=150, bbox_inches='tight')
-	plt.close()
-	'''
+	# fit_exp_category(args, normed = True, categories = l)
+	extract_lambda_feature(args, categories = l)
 
 if __name__ == '__main__':
 	main()
