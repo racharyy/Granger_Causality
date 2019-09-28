b9858441b655668ecaaff98dba24e4504035532d
Boyu Zhang <bzhang25@u.rochester.edu>
2019-09-27 20:49:43 -0400

    clean up

--- zipped_files_from_www/plot_helper.py
+++ zipped_files_from_www/plot_helper.py
@@ -7,6 +7,9 @@ from all_cls_result_lists import *
 #features = ['Lambda','Pretrained','Lambda+Self_Esteem']
  
 method_index = [0,1,2,5,7]
+methods = ["Logistic\nRegression", "Nearest\nNeighbors", "Linear\nSVM", "RBF SVM", "Gaussian Process",
+		 "Decision\nTree", "Random Forest", "Neural\nNet", "AdaBoost",
+		 "Naive Bayes", "QDA"]
 
 required_methods = required_list(methods,method_index)
 
@@ -23,33 +26,25 @@ m4 = required_list(lambda_and_se_results,method_index)
 #print(m1)
 plt.grid()
 
-r1 = ax.bar(xaxis, m2, bar_width,label = features[1],color = '#a6611a')
+r1 = ax.bar(xaxis, m2, bar_width,label = features[1], color = '#a6611a')
 
-r2 = ax.bar(xaxis + bar_width, m1, bar_width,label = features[0],color = '#dfc27d')
+r2 = ax.bar(xaxis + bar_width, m1, bar_width,label = features[0], color = '#dfc27d')
 
-r3 = ax.bar(xaxis + bar_width+ bar_width, m3, bar_width,label = features[2],color = '#80cdc1')
+r3 = ax.bar(xaxis + bar_width+ bar_width, m3, bar_width,label = features[2], color = '#80cdc1')
 
-r4 = ax.bar(xaxis + bar_width+ bar_width+ bar_width, m4, bar_width, label = features[3],color = '#018571')
+r4 = ax.bar(xaxis + bar_width+ bar_width+ bar_width, m4, bar_width, label = features[3], color = '#018571')
 
 ax.set_ylim([0, 0.75])
 
-# r1 = ax.barh(xaxis, m1, bar_width,label = features[0])
-
-# r2 = ax.barh(xaxis + bar_width, m2, bar_width,label = features[1])
-
-# r3 = ax.barh(xaxis + bar_width+ bar_width, m3, bar_width,label = features[2])
-
-# r4 = ax.barh(xaxis + bar_width+ bar_width+ bar_width, m4, bar_width, label = features[3])
-
-# ax.set_ylim([0, 1])
-
-ax.set_ylabel('Average F1 Score',fontsize=16)
-ax.set_title('F1 Score for different classifiers Using different features',fontsize=16)
-ax.set_xticks(xaxis+1.5*bar_width)
-ax.set_xticklabels(required_methods,fontsize=16)
+ax.set_ylabel('Average F1 Scores', fontsize=22)
+# ax.set_title('Performances of Different Classifiers and Features', fontsize=20)
+ax.set_xticks(xaxis + 1.5 * bar_width)
+plt.xticks(size = 22)
+plt.yticks(size = 22)
+ax.set_xticklabels(required_methods, fontsize=22)
 #ax.set_yticklabels(fontsize=18)
 #ax.text(2,0.8,'Lambda Feature is stronger')
-ax.legend(loc='lower right',fontsize=16)
+ax.legend(loc='lower right',fontsize=22)
 
 # f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
 
